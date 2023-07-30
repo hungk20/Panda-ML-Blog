6c89aa96844b387e7e2d83133241a8d5b766fe3f
Harry Dinh <hungk20@gmail.com>
2023-07-30 17:24:01 +0700

    add comment

--- chat_with_your_data/data_to_vectordb.py
+++ chat_with_your_data/data_to_vectordb.py
@@ -3,11 +3,12 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 
-DATA_STORE_DIR = "data_store"
 
+# (1) Document Loading
 loader = TextLoader("./born_pink_world_tour.md")
 docs = loader.load()
 
+# (2) Document Splitting
 chunk_size = 150
 chunk_overlap = 0
 text_splitter = RecursiveCharacterTextSplitter(
@@ -17,10 +18,11 @@ text_splitter = RecursiveCharacterTextSplitter(
 )
 split_docs = text_splitter.split_documents(docs)
 
+# (3) Store document and embeddings to vector store
 embedding = OpenAIEmbeddings()
 vectordb = FAISS.from_documents(
     documents=split_docs,
     embedding=embedding,
 )
-
+DATA_STORE_DIR = "data_store"
 vectordb.save_local(DATA_STORE_DIR)
